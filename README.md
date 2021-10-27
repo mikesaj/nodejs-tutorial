6a0f5636dea303c5899d3c76f90585bca77feae0
Michael <michael.saj@gmail.com>
2021-10-27 10:12:06 -0400

    added GCP storage upload function

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Image Thumbnail Generator Service
+
+
+
+## Startup Server in Development Node
+run `npm run start:dev` in your terminal
+
+### NodeMon pre-requisite
+install nodemone globally on your machine 
+run `npm install nodemon -g`
--- app.js
+++ app.js
@@ -2,15 +2,29 @@ const express = require("express");
 
 // const path = require("path");
 const logger = require("morgan");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 
+dotenv.config();
 const index = require("./routes/index");
 const users = require("./routes/users");
+const upload = require("./routes/upload");
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set morgan to log differently depending on environment
+if (app.get("env") !== "production") {
+  app.use(logger("common"));
+} else {
+  app.use(logger("dev"));
+}
+
 app.use("/", index);
 app.use("/users", users);
+app.use("/upload", upload);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -26,7 +40,7 @@ app.use((err, req, res) => {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error response
-  res.status(err.status || 500).json("Error1");
+  res.status(err.status || 500);
 });
 
 module.exports = app;
